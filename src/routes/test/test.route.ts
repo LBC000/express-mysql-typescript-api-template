7c694f977d2b241cf4656b877ce506a1bed038a1
ooo <wu>
2023-05-26 10:33:46 +0800

    优化

--- src/routes/test/test.route.ts
+++ src/routes/test/test.route.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import apiResponse from '../../utilities/apiResponse';
 import httpStatusCodes from 'http-status-codes';
+import { getRepository } from 'typeorm';
+import { User } from '../../entities/user/user.entity';
 
 const router = express.Router();
 
@@ -13,12 +15,15 @@ router.get(
   '/getList',
   celebrate({
     [Segments.QUERY]: {
-      id: Joi.string().required(),
+      id: Joi.string(),
     },
   }),
-  (req, res) => {
-    console.log(req.query, '测试');
-    return apiResponse.result(res, { a: '测试' }, httpStatusCodes.OK);
+  async (req, res) => {
+    let data = await getRepository(User).find(req.query);
+
+    console.log(req.query, data, '测试');
+
+    return apiResponse.result(res, { list: data }, httpStatusCodes.OK);
   },
 );
 
